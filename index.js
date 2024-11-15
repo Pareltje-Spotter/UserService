64825ec11d99a73fb45e4d325b7871550e6e0312
FontysNiels <nielsfeijen76@gmail.com>
2024-11-15 12:26:04 +0100

    make sure it runs duhh

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 // const MongoClient = require('mongodb').MongoClient;
 // const ObjectID = require('mongodb').ObjectId;
 
-// const app = express()
+const app = express()
 // app.use(cors());
 // app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
