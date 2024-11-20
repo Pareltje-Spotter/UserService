527645ca450fa586138caae7b17cab40efc132fa
FontysNiels <nielsfeijen76@gmail.com>
2024-11-20 11:24:28 +0100

    teste

--- db.js
+++ db.js
@@ -4,9 +4,9 @@ const { Pool } = require('pg');
 const pool = new Pool({
   host: 'postgres',
   port: 5432,
-  user: 'user',
-  password: 'admin',
-  database: 'userinfo',
+  user: process.env.POSTGRES_USER || 'user', // Set the username (environment variable or default)
+  password: process.env.POSTGRES_PASSWORD || 'admin', // Set the password (environment variable or default)
+  database: process.env.POSTGRES_DB || 'userinfo', // Set the database (environment variable or default)
 });
 
 module.exports = pool;
