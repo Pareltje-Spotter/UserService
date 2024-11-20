89540a9f06a0dc2165d651e570f6dc1e178b7f6d
FontysNiels <nielsfeijen76@gmail.com>
2024-11-20 11:27:18 +0100

    dbcon change

--- db.js
+++ db.js
@@ -2,11 +2,11 @@ const { Pool } = require('pg');
 
 // Configure the PostgreSQL client
 const pool = new Pool({
-  host: 'postgres',
+  host: process.env.POSTGRES_HOSTNAME || 'localhost',
   port: 5432,
-  user: process.env.POSTGRES_USER || 'user', // Set the username (environment variable or default)
-  password: process.env.POSTGRES_PASSWORD || 'admin', // Set the password (environment variable or default)
-  database: process.env.POSTGRES_DB || 'userinfo', // Set the database (environment variable or default)
+  user: process.env.POSTGRES_USER || 'user', 
+  password: process.env.POSTGRES_PASSWORD || 'admin', 
+  database: process.env.POSTGRES_DB || 'userinfo', 
 });
 
 module.exports = pool;
