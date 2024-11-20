d45aecd16777aa9b9f6473cd425cc1117602aa02
FontysNiels <nielsfeijen76@gmail.com>
2024-11-20 11:30:41 +0100

    ff wel goed zo

--- db.js
+++ db.js
@@ -2,11 +2,17 @@ const { Pool } = require('pg');
 
 // Configure the PostgreSQL client
 const pool = new Pool({
-  host: process.env.POSTGRES_HOSTNAME || 'localhost',
+  host: 'postgres',
   port: 5432,
-  user: process.env.POSTGRES_USER || 'user', 
-  password: process.env.POSTGRES_PASSWORD || 'admin', 
-  database: process.env.POSTGRES_DB || 'userinfo', 
+  user: 'user',
+  password: 'admin',
+  database: 'userinfo',
 });
 
+
+// host: process.env.POSTGRES_HOSTNAME || 'localhost',
+// port: 5432,
+// user: process.env.POSTGRES_USER || 'user', 
+// password: process.env.POSTGRES_PASSWORD || 'admin', 
+// database: process.env.POSTGRES_DB || 'userinfo', 
 module.exports = pool;
