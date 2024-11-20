5ac757ed1be13efa381d4f826f05c83597730249
FontysNiels <nielsfeijen76@gmail.com>
2024-11-20 11:21:04 +0100

    change back

--- db.js
+++ db.js
@@ -1,21 +1,12 @@
 const { Pool } = require('pg');
 
-// Create a client instance and configure it with your PostgreSQL connection details
-const client = new Client({
-    host: 'postgres',      // This is the name of the Kubernetes service
-    port: 5432,            // Default PostgreSQL port
-    user: process.env.POSTGRES_USER || 'user', // Set the username (environment variable or default)
-    password: process.env.POSTGRES_PASSWORD || 'admin', // Set the password (environment variable or default)
-    database: process.env.POSTGRES_DB || 'userinfo', // Set the database (environment variable or default)
+// Configure the PostgreSQL client
+const pool = new Pool({
+  host: 'postgres',
+  port: 5432,
+  user: 'user',
+  password: 'admin',
+  database: 'userinfo',
 });
 
-// Connect to the PostgreSQL database
-client.connect()
-    .then(() => {
-        console.log('Connected to PostgreSQL');
-    })
-    .catch((err) => {
-        console.error('Error connecting to PostgreSQL', err.stack);
-    });
-
-module.exports = client;
+module.exports = pool;
