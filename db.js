5a3ac26a7c66c8d2acac2669b8acc7e3b743ad1b
FontysNiels <nielsfeijen76@gmail.com>
2024-11-20 11:06:51 +0100

    hostname change

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 
 // Configure the PostgreSQL client
 const pool = new Pool({
-  host: 'localhost',
+  host: 'postgres',
   port: 5432,
   user: 'user',
   password: 'admin',
